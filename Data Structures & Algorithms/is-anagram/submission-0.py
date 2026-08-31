f9d95999a61ecccd829ba693057e45c0bfebe3ee
Đình Nhân <nhanho0109@gmail.com>
2026-08-31 21:43:26 +0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen_s = {}
+        for word in s:
+            seen_s[word] = seen_s.get(word, 0) + 1
+
+        seen_t = {}
+        for word in t:
+            seen_t[word] = seen_t.get(word, 0) + 1
+
+        return seen_s == seen_t
